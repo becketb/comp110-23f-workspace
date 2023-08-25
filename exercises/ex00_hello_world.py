6255cc1c7b69cce2d528f98dcf0070496882891d
becketb <bbradley@unc.edu>
2023-08-25 12:07:02 -0400

    Completed Exercise 00

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,6 +1,5 @@
-"""My first program for COMP110"""
+"""My first program for COMP110."""
 
-_author_="730529193"
+__author__ = "730529193"
 
 print("Hello, World")
-print("Hello, to everyone, the world included.")
