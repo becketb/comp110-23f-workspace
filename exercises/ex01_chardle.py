d23b7d8f3ffeb709d33d955ef6f66a0cf61c5f2c
becketb <bbradley@unc.edu>
2023-09-13 11:30:52 -0400

    Completed Chardle

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,4 +1,4 @@
-"""EX01 - Chardle - First step in toppling The New York Times (satire)."""
+"""EX01 - Chardle - A cute step toward Wordle."""
 
 __author__ = "730529193"
 
@@ -7,43 +7,40 @@ five_word: str = input("Enter a 5-character word: ")
 if len(five_word) != 5:
     print("Error: Word must contain 5 characters")
     exit()
-else:
-    print(five_word)
 # if the character guessed is unequal to 1 there is an error message
 single_character: str = input("Enter a single character: ")
 if len(single_character) != 1:
     print("Error: Character must be a single character.")
     exit()
 else: 
-    print(single_character)
     print("Searching for " + single_character + " in " + five_word)
 # code for instances and matching indices
 instance = 0
 matching = False
 if five_word[0] == single_character:
     instance = instance + 1
-    print(single_character + " found at index 0. ")
+    print(single_character + " found at index 0.")
     matching = False
 if five_word[1] == single_character:
     instance = instance + 1
-    print(single_character + " found at index 1. ")
+    print(single_character + " found at index 1.")
     matching = True
 if five_word[2] == single_character:
     instance = instance + 1
-    print(single_character + " found at index 2. ")
+    print(single_character + " found at index 2.")
     matching = True
 if five_word[3] == single_character:
     instance = instance + 1
-    print(single_character + " found at index 3. ")
+    print(single_character + " found at index 3.")
     matching = True
 if five_word[4] == single_character:
     instance = instance + 1
-    print(single_character + " found at index 4. ")
+    print(single_character + " found at index 4.")
     matching = True
 if instance == 1:
     print(str(instance) + " instance of " + single_character + " found in " + five_word)
 else:
     if instance > 1:
         print(str(instance) + " instances of " + single_character + " found in " + five_word)    
-if matching is not True:
+if instance == 0:
     print("No instances of " + single_character + " found in " + five_word)
